dd211270e1212906dc4ea611c3b2c31ae3f3ccf0
nikolay-olkhovsky <nikolay.olkhovsky@gmail.com>
2024-07-19 19:21:25 +0500

    Implement unittest and fix errors.

--- areas.py
+++ areas.py
@@ -1,9 +1,15 @@
 import math
 
 def circleArea(r):
+    if r < 0. : 
+        raise ValueError("Radius r cannot be < 0. (current value {0})".format(r))
     return math.pi * r ** 2
 
 def triangleArea(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        raise ValueError("A triangle side cannot be <= 0. (current values a={0}, b={1}, c={2})".format(a, b, c))
+    if a >= b+c or b >= a+c or c >= a+b:
+        raise ValueError("A triangle side cannot be greater or equal to the sum of other sides (current values a={0}, b={1}, c={2})".format(a, b, c))
     if a > b:
         if a > c and a**2 == b**2 + c**2:
             return (b * c) / 2.
